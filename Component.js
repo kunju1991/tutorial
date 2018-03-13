ebebc47e3fa3cea6bce368d59a450e5c96111efa
kunj bihari shukla <kunjshukla@hotmail.com>
2018-03-13 20:31:22 +0530

    initial project version

---
+++ Component.js
@@ -0,0 +1,18 @@
+sap.ui.define([
+               "sap/ui/core/UIComponent",
+               "sap/ui/model/json/JSONModel",
+               "sap/ui/Device"
+],function(UIComponent, JSONModel, Device){
+	"use strict";
+	
+	return UIComponent.extend("com.tutorial.Component",{
+		
+		metadata: {
+			manifest: "json"
+		},
+		init:function(){
+			UIComponent.prototype.init.apply(this, arguments);
+			this.getRouter().initialize();
+		}
+	});
+});
