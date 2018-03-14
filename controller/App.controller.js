21ba09908e6ecf0bf04a5a730738d5c7068f2ac2
kunj bihari shukla <kunjshukla@hotmail.com>
2018-03-14 16:16:45 +0530

    Eclipse COmmit and PUsh2

--- controller/App.controller.js
+++ controller/App.controller.js
@@ -23,9 +23,9 @@ sap.ui.define([
 		* (NOT before the first rendering! onInit() is used for that one!).
 		* @memberOf tutorial.App
 		*/
-//			onBeforeRendering: function() {
-		//
-//			},
+			onBeforeRendering: function() {
+		
+			},
 
 		/**
 		* Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
