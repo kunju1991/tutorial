19898fc973bba3ec4ef257a9e6fa48777199b0dc
kunj bihari shukla <kunjshukla@hotmail.com>
2018-03-14 16:11:53 +0530

    Commit from GITGUI

--- controller/Master.controller.js
+++ controller/Master.controller.js
@@ -5,9 +5,9 @@ sap.ui.controller("view.Master", {
 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
 * @memberOf view.Master
 */
-//	onInit: function() {
-//
-//	},
+	onInit: function() {
+
+	},
 
 /**
 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
